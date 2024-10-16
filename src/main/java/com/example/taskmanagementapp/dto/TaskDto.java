aad87f77858237aa705f56c5eca9579339c2a3a1
Cem Yılmaz <yilmazcem1122@gmail.com>
2024-10-17 01:11:12 +0300

    DTO Service is created

--- src/main/java/com/example/taskmanagementapp/dto/TaskDto.java
+++ src/main/java/com/example/taskmanagementapp/dto/TaskDto.java
@@ -1,5 +1,8 @@
 package com.example.taskmanagementapp.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Data
@@ -9,6 +12,7 @@ import lombok.*;
 @Setter
 
 public class TaskDto {
+
 
     private Long id;
 
--- src/main/java/com/example/taskmanagementapp/service/UserService.java
+++ src/main/java/com/example/taskmanagementapp/service/UserService.java
@@ -1,10 +1,17 @@
 package com.example.taskmanagementapp.service;
 
+import com.example.taskmanagementapp.dto.UserDto;
+import com.example.taskmanagementapp.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
 
-
-
+    public UserDto createUser(UserDto userDto);
+    public UserDto updateUser(UserDto userDto);
+    public UserDto getUserById(Long id);
+    public List<UserDto> getAllUsers();
+    public void deleteUserById(Long id);
 }
